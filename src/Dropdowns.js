c37f29bc0420329fe9d8709b82430dd6987e9538
fangjjcs <grace963214789@gmail.com>
2021-10-22 23:00:27 +0800

    select all

--- src/Dropdowns.js
+++ src/Dropdowns.js
@@ -5,15 +5,24 @@ import { useEffect, useState } from "react";
 export default function Dropdowns(props) {
   
     const [selected, setselected] = useState(props.selected);
+    const [isAll, setAll] = useState(false);
 
     useEffect(()=>{
         setselected(props.selected)
     }, [props.selected])
 
+    const options = [{ title: "all", group: null , id: "all"}, ...props.list]
 
     const onChangeHandler =(event, value)=>{
-        console.log(value)
-        props.onChange(props.name, value);
+        // console.log(value)
+        if(value.length>0 && value.findIndex(obj=>obj.id === 'all') > -1 ){
+            props.onChange(props.name, props.list);
+            setAll(true)
+        }else{
+            props.onChange(props.name, value);
+            setAll(false);
+        }
+        
     }
   
     return (
@@ -21,7 +30,7 @@ export default function Dropdowns(props) {
       multiple
       style={{minWidth: 150, margin: 8}}
       id="tags-standard"
-      options={props.list}
+      options={options}
       groupBy={(option) => option.group}
       getOptionLabel={(option) => option.title}
       //key={props.selected} //   when key value changed, component will re-render
